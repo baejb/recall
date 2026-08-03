9d2a9a12e78d1dce00524562c7f0a85ee9511e45
HYERINI <rinlee2001@naver.com>
2026-08-03 11:11:29 +0900

    refactor: WebConfig을 common 패키지로 이동 (아키텍처 정합)
    skeleton이 com.recall 루트로 옮긴 WebConfig을 docs/architecture.md의 common/ 규칙에 맞춰 되돌림.

--- backend/src/main/java/com/recall/common/WebConfig.java
+++ backend/src/main/java/com/recall/common/WebConfig.java
@@ -1,4 +1,4 @@
-package com.recall;
+package com.recall.common;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
